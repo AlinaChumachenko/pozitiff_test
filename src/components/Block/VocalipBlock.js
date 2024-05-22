75f27a14cd8b9915d9b5b8392ae69f086fadc698
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-22 09:38:44 +0300

    bag fix

--- src/components/Block/VocalipBlock.js
+++ src/components/Block/VocalipBlock.js
@@ -1,9 +1,8 @@
-import ButtonPopup from "../Button/ButtonPopup.js";
+// import ButtonPopup from "../Button/ButtonPopup.js";
 
 export const VocalipBlock = () => {
   return (
     <div>
-      <ButtonPopup />
       <h2>VOCALIP</h2>
       <img src="../../../public/ill_hero.png" alt="" width={555} />
     </div>
--- src/components/Button/ButtonPopup.js
+++ src/components/Button/ButtonPopup.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { ReactComponent as IconPlas } from "../../../public/plus.svg";
+// import React from "react";
+// import { ReactComponent as IconPlas } from "../../../public/plus.svg";
 
-const ButtonPopup = () => {
-  return (
-    <div>
-      <button>
-        <IconPlas />
-      </button>
-    </div>
-  );
-};
-export default ButtonPopup;
+// const ButtonPopup = () => {
+//   return (
+//     <div>
+//       <button>
+//         <IconPlas />
+//       </button>
+//     </div>
+//   );
+// };
+// export default ButtonPopup;
