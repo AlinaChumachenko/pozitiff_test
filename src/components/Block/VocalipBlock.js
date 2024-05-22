72e9e11bc46805bc0efef43fe5f1b3923b6387e1
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-22 09:52:08 +0300

    imagas

--- src/components/Block/VocalipBlock.js
+++ src/components/Block/VocalipBlock.js
@@ -4,7 +4,7 @@ export const VocalipBlock = () => {
   return (
     <div>
       <h2>VOCALIP</h2>
-      <img src="../../../public/ill_hero.png" alt="" width={555} />
+      <img src="../../imagas/ill_hero.png" alt="" width={555} />
     </div>
   );
 };
