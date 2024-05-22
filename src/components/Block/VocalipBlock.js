c746e697ab53d50841ba17fc008c64fcf4b72320
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-22 10:22:21 +0300

    iamegas

--- src/components/Block/VocalipBlock.js
+++ src/components/Block/VocalipBlock.js
@@ -1,16 +1,12 @@
 import Image from "next/image";
+import background from "path";
 // import ButtonPopup from "../Button/ButtonPopup.js";
 
 export const VocalipBlock = () => {
   return (
     <div>
       <h2>VOCALIP</h2>
-      <Image
-        src="../../images/images/ill_hero.png"
-        alt="Schedule"
-        width={555}
-        height={226}
-      />
+      <Image src={background} alt="Schedule" width={555} height={226} />
     </div>
   );
 };
