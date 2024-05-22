a4e633e4657bf5f57468689a72eb542b71daa20d
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-22 22:17:21 +0300

    Show PopUp

--- src/components/Button/ButtonPopup.js
+++ src/components/Button/ButtonPopup.js
@@ -1,12 +1,22 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import IconPlus from "@/images/plus.svg";
+import { CoralPopup } from "@/components/Popup/CoralPopup.js";
 
 export const ButtonPopup = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsPopupOpen(true);
+  };
+
   return (
     <div>
-      <button type="button">
+      <button type="button" onClick={handleButtonClick} width={50} height={50}>
         <Image priority src={IconPlus} alt="Plus" />
       </button>
+      {isPopupOpen && <CoralPopup />}
     </div>
   );
 };
--- src/components/Popup/CoralPopup.js
+++ src/components/Popup/CoralPopup.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { ButtonClosePopup } from "@/components/Button/ButtonClosePopup.js";
 import { ButtonProdject } from "@/components/Button/ButtonProdject.js";
